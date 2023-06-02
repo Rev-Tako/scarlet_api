990a7f39f542845d60c5201a622b8ba585fcd468
RevTako <turo15@icloud.com>
2023-06-02 16:21:20 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
+import React, { Component } from "react"
 import logo from './logo.svg';
 import './App.css';
+
+class SCARLET_API extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {}
+
+  }
+  //functions go here
+    async io_backend() {
+
+    }
+
+  render() {
+    return(
+        <div>
+          Script calls go here?
+        </div>
+    )
+  }
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        This should be blank
       </header>
+    < SCARLET_API />
     </div>
   );
 }
